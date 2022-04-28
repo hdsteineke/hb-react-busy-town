c219db03867021c9ec401faab2f7e9f1c9de8bcb
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-04-27 17:27:29 -0700

    filled out vehicle.js with vehicle emojis and string

--- src/TrafficLight.js
+++ src/TrafficLight.js
@@ -1,21 +1,6 @@
 import React, { useState } from 'react';
 
 export default function TrafficLight({ color }) {
-  // const [isRed, setIsRed] = useState(true);
-  // const [isYellow, setIsYellow] = useState(false);
-  // const [isGreen, setIsGreen] = useState(false);
-
-  // function handleRedLight() {
-  //   setIsRed(true) && setIsYellow(false) && setIsGreen(false);
-  // }
-
-  // function handleYellowLight() {
-  //   setIsRed(false) && setIsYellow(true) && setIsGreen(false);
-  // }
-
-  // function handleGreenLight() {
-  //   setIsRed(false) && setIsYellow(false) && setIsGreen(true);
-  // }
 
   return (
     // this component takes in a prop called color. It is either 'red', 'yellow' or 'green'.
@@ -23,8 +8,6 @@ export default function TrafficLight({ color }) {
       {/* in each of these divs, set it up so that if the color matches, add the css class 'on' to the classNames */}
       {/* for example, the css classes for the div below should be `red circle on`, if props.color is 'red' */}
       <div className={`red circle ${color === 'red' && 'on'}`}>{
-        
-
       }
 
       </div>
